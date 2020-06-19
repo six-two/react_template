81c1f18dca0a3c5a12bc7e6a67c3497c4024da28
six-two <info@six-two.dev>
2020-06-19 15:40:40 +1200

    Status: Sucessfully processes template and stores it in temporary folder

--- main.py
+++ main.py
@@ -4,6 +4,13 @@ import os
 import yaml
 from liquid import Liquid
 import shutil
+from munch import munchify
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+LIQUID_FILE_EXTENSION = ".liquid"
+CODEC = "utf-8"
+# Debugging switches
+DONT_WRITE_FILES = False
 
 # Overwrite files, which might result in changes made by the user being lost
 OVERWRITE_OPTION = "--force"
@@ -16,8 +23,7 @@ class TemplateBuilder:
     def __init__(self, reactFolder, ask, overwrite):
         self.tmpFolder = TMP_DIR
         self.reactFolder = reactFolder
-        scriptDir = os.path.dirname(os.path.realpath(__file__))
-        self.templateFolder = os.path.join(scriptDir, "template")
+        self.templateFolder = os.path.join(SCRIPT_DIR, "template")
         self.ask = ask
         self.overwrite = overwrite
 
@@ -25,7 +31,10 @@ class TemplateBuilder:
         # TODO test if react folder exists
 
         # clean the tmp folder
-        shutil.rmtree(self.tmpFolder)
+        try:
+            shutil.rmtree(self.tmpFolder)
+        except:
+            pass
         os.makedirs(self.tmpFolder)
 
         if self.overwrite:
@@ -33,35 +42,17 @@ class TemplateBuilder:
             print("Backup or commit your code before executing this action!");
             print("Do you really want to force overwrites on all existing files? [yes/NO]")
             a = input();
-            if not a.toLower().startsWith("y"):
+            if not a.lower().startswith("y"):
                 print("Aborted")
                 return
 
-        print("TODO implement!")
         # step one: process all files into a tmp directory
-        self.processTemplateFiles()
+        yamlPath = os.path.join(self.reactFolder, "react-template.yaml")
+        Preprocessor(yamlPath).processFolder(self.templateFolder, self.tmpFolder)
         # step 2: compare files in tmp dir and react dir
         changes = self.getChangedFileList()
         # step 3: replace files that have changed
         self.replaceFiles(changes)
-
-    def processTemplateFiles(self):
-        yamlPath = os.path.join(self.reactFolder, "react-template.yaml")
-        with open(yamlPath, "r") as f:
-            yamlData = yaml.safe_load(f.read())
-
-        print(yamlData)
-
-        pass
-
-    def processFile(self, relPath, yamlData):
-        inputFile = os.path.join(self.templateFolder, relPath)
-        outputFile = os.path.join(self.tmpFolder, relPath)
-        liq = Liquid(path, liquid_from_file=True)
-        ret = liq.render(site=yamlData)
-        print(ret)
-        with open(outputFile, "w") as f:
-            f.write(ret)
 
     def getChangedFileList(self):#returns [(relPath, change)]
         pass
@@ -73,6 +64,64 @@ class TemplateBuilder:
 
     def confirmChanges(self, changedFileList):
         pass
+
+
+class Preprocessor:
+    def __init__(self, yamlPath):
+        yamlText = readFileBytes(yamlPath).decode(CODEC)
+        # use munchify so that keys can be accessed using the dot notation
+        self.yamlData = munchify(yaml.safe_load(yamlText))
+
+    def processFolder(self, inputFolder, outputFolder):
+        for root, dirs, files in os.walk(inputFolder):
+           for name in files:
+              inputFile = os.path.join(root, name)
+              relPath = removePathPrefix(inputFile, inputFolder)
+              outputFile = os.path.join(outputFolder, relPath)
+              self.processFile(inputFile, outputFile)
+
+    def processFile(self, inputFile, outputFile):
+        fileBytes = readFileBytes(inputFile)
+
+        if inputFile.endswith(LIQUID_FILE_EXTENSION):
+            print("Processing '{}'".format(inputFile))
+            # remove liquid extension
+            outputFile = outputFile[:-len(LIQUID_FILE_EXTENSION)]
+            # process with liquid
+            fileAsString = fileBytes.decode(CODEC)
+            processedString = Liquid(fileAsString).render(site=self.yamlData)
+            fileBytes = processedString.encode(CODEC)
+
+        writeFileBytes(outputFile, fileBytes)
+
+
+def removePathPrefix(path, prefixToRemove):
+    # remove prefix
+    path = path[len(prefixToRemove):]
+    # remove leading slashes
+    while os.path.isabs(path):
+        path = path[1:]
+    return path
+
+
+def writeFileBytes(path, content):
+    # create the parent folder if it did not exist
+    if (DONT_WRITE_FILES):
+        print("Write prevented: '{}'".format(path))
+        return
+
+    try:
+        os.makedirs(os.path.dirname(path))
+    except:
+        pass
+
+    with open(path, "wb") as f:
+        f.write(content)
+
+def readFileBytes(path):
+    with open(path, "rb") as f:
+        fileBytes = f.read()
+    return fileBytes
 
 
 if __name__ == "__main__":
