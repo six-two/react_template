ff0de54a04b84e05f98bc37728df96640cd46eea
six-two <git@six-two.dev>
2021-01-18 18:15:50 +1300

    Implemented support for custom build commands

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ import os
 import yaml
 from liquid import Liquid
 import shutil
-from munch import munchify, DefaultMunch
 import subprocess
+# External library
+from munch import munchify, DefaultMunch
 
+CONFIG_FILE_NAME = "react-template.yaml"
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+PRE_BUILD_COMMANDS = "pre_build"
+POST_BUILD_COMMANDS = "post_build"
 LIQUID_FILE_EXTENSION = ".liquid"
 CODEC = "utf-8"
 # Debugging switches
@@ -19,6 +23,7 @@ OVERWRITE_OPTION = "--force"
 # Before executing the changes, print a summary and ask the user for confirmation
 ASK_OPTION = "--ask"
 # The dir to temporarily store the processed template files in
+TMP_DIR_RUN_FIRST_OUTPUT = "/tmp/react_template_run_first_output"
 TMP_DIR = "/tmp/react_template"
 # Compress html files
 COMPRESS_HTML = True
@@ -26,43 +31,57 @@ COMPRESS_HTML = True
 if COMPRESS_HTML:
     import htmlmin
 
+
+def rm_folder(path):
+    try:
+        shutil.rmtree(path)
+    except:
+        pass
+
+
+def empty_folder(path):
+    rm_folder(path)
+    os.makedirs(path)
+
+
 class TemplateBuilder:
     def __init__(self, reactFolder, ask, overwrite):
-        self.tmpFolder = TMP_DIR
         self.reactFolder = reactFolder
-        self.templateFolder = os.path.join(SCRIPT_DIR, "template")
         self.ask = ask
         self.overwrite = overwrite
 
     def run(self):
         # TODO test if react folder exists
 
-        # clean the tmp folder
-        try:
-            shutil.rmtree(self.tmpFolder)
-        except:
-            pass
-        os.makedirs(self.tmpFolder)
+        empty_folder(TMP_DIR)
 
         if self.overwrite and CONFIRM_FORCE_OPTION:
             print("You are using the '{}' option, which might result in data loss")
             print("Backup or commit your code before executing this action!")
-            print("Do you really want to force overwrites on all existing files? [y/N]")
+            print(
+                "Do you really want to force overwrites on all existing files? [y/N]")
             a = input()
             if not a.lower().startswith("y"):
                 print("Aborted")
                 return
 
-        # step one: process all files into a tmp directory
-        yamlPath = os.path.join(self.reactFolder, "react-template.yaml")
-        Preprocessor(yamlPath).processFolder(self.templateFolder, self.tmpFolder)
+        # step 1: build the template
+        templateFolder = os.path.join(SCRIPT_DIR, "template")
+        # Run pre build commands
+        templateFolder = run_pre_build_commands(
+            templateFolder, self.reactFolder)
+        # process template files
+        Preprocessor(self.reactFolder).processFolder(templateFolder, TMP_DIR)
+        # Run post build commands
+        run_post_build_commands(TMP_DIR, self.reactFolder)
+
         # step 2: compare files in tmp dir and react dir
         changes = self.getChangedFileList()
         # step 3: replace files that have changed
         self.replaceFiles(changes)
 
     def getChangedFileList(self):
-        changes = FolderCompare.compareFolders(self.tmpFolder, self.reactFolder)
+        changes = FolderCompare.compareFolders(TMP_DIR, self.reactFolder)
 
         statusList = [FolderCompare.ADD]
         if self.overwrite:
@@ -76,7 +95,7 @@ class TemplateBuilder:
             self.confirmChanges(changedFileList)
 
         for relPath in changedFileList:
-            inputFile = os.path.join(self.tmpFolder, relPath)
+            inputFile = os.path.join(TMP_DIR, relPath)
             outputFile = os.path.join(self.reactFolder, relPath)
             data = readFileBytes(inputFile)
             writeFileBytes(outputFile, data)
@@ -97,6 +116,58 @@ class TemplateBuilder:
             print("No files will be changed")
 
 
+# Returns: the path to the updated template
+def run_pre_build_commands(templateFolder, projectFolder):
+    config = parse_config_file(projectFolder)
+    build_commands = config.get(PRE_BUILD_COMMANDS)
+
+    if build_commands:
+        tmp_dir = TMP_DIR_RUN_FIRST_OUTPUT
+
+        # Remove folder (if exists) and create parents (if needed)
+        empty_folder(tmp_dir)
+        rm_folder(tmp_dir)
+        # Copy the template to the tmp dir
+        shutil.copytree(templateFolder, tmp_dir)
+
+        run_commands(tmp_dir, projectFolder, build_commands)
+
+        return tmp_dir
+    else:
+        print("No custom build commands given")
+        return templateFolder
+
+
+def run_post_build_commands(template_dir, project_dir):
+    config = parse_config_file(project_dir)
+    build_commands = config.get(POST_BUILD_COMMANDS)
+
+    if build_commands:
+        run_commands(template_dir, project_dir, build_commands)
+
+def parse_config_file(project_dir):
+    yamlPath = os.path.join(project_dir, CONFIG_FILE_NAME)
+    yamlText = readFileBytes(yamlPath).decode(CODEC)
+    return yaml.safe_load(yamlText)
+
+
+def run_commands(template_dir, project_dir, commands):
+    # Change into the template directory
+    old_cwd = os.getcwd()
+    os.chdir(template_dir)
+
+    # Run the build commands in the copy
+    absolute_project_dir = os.path.join(old_cwd, project_dir)
+    for command in commands:
+        command = str(command).replace("<PROJECT>", absolute_project_dir)
+        print(" Build command ".center(80, "="))
+        print(f"Executing: '{command}'")
+        subprocess.call(command, shell=True)
+
+    # Change back to the old working dir
+    os.chdir(old_cwd)
+
+
 class FolderCompare:
     SAME = "SAME"
     ADD = "ADD"
@@ -114,13 +185,14 @@ class FolderCompare:
     def compareFolders(sourceFolder, destFolder):
         diff = {}
         for root, _dirs, files in os.walk(sourceFolder):
-           for name in files:
-              sourceFile = os.path.join(root, name)
-              relPath = removePathPrefix(sourceFile, sourceFolder)
-              destFile = os.path.join(destFolder, relPath)
+            for name in files:
+                sourceFile = os.path.join(root, name)
+                relPath = removePathPrefix(sourceFile, sourceFolder)
+                destFile = os.path.join(destFolder, relPath)
 
-              status = FolderCompare.compareFileContents(sourceFile, destFile)
-              diff[relPath] = status
+                status = FolderCompare.compareFileContents(
+                    sourceFile, destFile)
+                diff[relPath] = status
         return diff
 
     @staticmethod
@@ -137,18 +209,18 @@ class FolderCompare:
 
 
 class Preprocessor:
-    def __init__(self, yamlPath):
-        yamlText = readFileBytes(yamlPath).decode(CODEC)
+    def __init__(self, project_dir):
+        config = parse_config_file(project_dir)
         # use munchify so that keys can be accessed using the dot notation
-        self.yamlData = DefaultMunch.fromDict(munchify(yaml.safe_load(yamlText)), "")
+        self.yamlData = DefaultMunch.fromDict(munchify(config), "")
 
     def processFolder(self, inputFolder, outputFolder):
         for root, _dirs, files in os.walk(inputFolder):
-           for name in files:
-              inputFile = os.path.join(root, name)
-              relPath = removePathPrefix(inputFile, inputFolder)
-              outputFile = os.path.join(outputFolder, relPath)
-              self.processFile(inputFile, outputFile)
+            for name in files:
+                inputFile = os.path.join(root, name)
+                relPath = removePathPrefix(inputFile, inputFolder)
+                outputFile = os.path.join(outputFolder, relPath)
+                self.processFile(inputFile, outputFile)
 
     def processFile(self, inputFile, outputFile):
         fileBytes = readFileBytes(inputFile)
@@ -165,24 +237,25 @@ class Preprocessor:
             inputFolder, inputFileName = os.path.split(inputFile)
             if inputFileName.startswith("_"):
                 print("Ignoring '{}'".format(inputFile))
-                return # do not process the file
+                return  # do not process the file
             print("Processing '{}'".format(inputFile))
             outputFile = outputFile[:-5] + ".css"
             importFolder = inputFolder  # Maybe change this?
             subprocess.run(["sassc",
-              "--style", "compressed",
-              "--load-path", importFolder,
-               inputFile, outputFile
-            ])
+                            "--style", "compressed",
+                            "--load-path", importFolder,
+                            inputFile, outputFile
+                            ])
             return
 
         if COMPRESS_HTML and (outputFile.endswith(".htm") or outputFile.endswith(".html")):
             fileAsString = fileBytes.decode(CODEC)
             processedString = htmlmin.minify(fileAsString,
-                remove_comments=True, remove_empty_space=True) + "\n"
+                                             remove_comments=True, remove_empty_space=True) + "\n"
             fileBytes = processedString.encode(CODEC)
 
         writeFileBytes(outputFile, fileBytes)
+
 
 def removePathPrefix(path, prefixToRemove):
     # remove prefix
@@ -207,6 +280,7 @@ def writeFileBytes(path, content):
     with open(path, "wb") as f:
         f.write(content)
 
+
 def readFileBytes(path):
     with open(path, "rb") as f:
         fileBytes = f.read()
@@ -215,7 +289,8 @@ def readFileBytes(path):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: {} <react_project_folder> [{}] [{}]".format(sys.argv[0], ASK_OPTION, OVERWRITE_OPTION))
+        print("Usage: {} <react_project_folder> [{}] [{}]".format(
+            sys.argv[0], ASK_OPTION, OVERWRITE_OPTION))
         sys.exit(1)
 
     reactFolder = sys.argv[1]
