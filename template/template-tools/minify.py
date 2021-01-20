b40baefe5a93caabc89aba0c886777755d53f6c6
six-two <git@six-two.dev>
2021-01-20 15:39:36 +1300

    Added CSS and JS minification to minify.py

--- template/template-tools/minify.py
+++ template/template-tools/minify.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 
 # ================== i18n.py =====================
-# Minifies HTML files (and in the future maybe css and js too).
+# Minifies HTML, CSS, and JS files
 # Hook type: post_build
 # Configuration: You need to do nothing :)
 
 import os
 # External dependencies
 import htmlmin
+import rcssmin
+import rjsmin
 
 CODEC = "utf-8"
 
@@ -23,23 +25,38 @@ def replace_file_contents(path, fn):
         f.write(file_contents.encode(CODEC))
 
 
-def minify_html(file_contents):
-            return htmlmin.minify(file_contents,
-                                  remove_comments=True,
-                                  remove_empty_space=True) + "\n"
+def minify_html(file_contents: str) -> str:
+    return htmlmin.minify(file_contents,
+                          remove_comments=True,
+                          remove_empty_space=True) + "\n"
+
+
+def minify_css(file_contents: str) -> str:
+    return rcssmin.cssmin(file_contents)
+
+
+def minify_js(file_contents: str) -> str:
+    return rjsmin.jsmin(file_contents)
+
 
 def process_file(file_path):
     if file_path.endswith(".htm") or file_path.endswith(".html"):
-        # Minify the html file
         print(f"Minifying {file_path}")
-
         replace_file_contents(file_path, minify_html)
+    elif file_path.endswith(".css"):
+        print(f"Minifying {file_path}")
+        replace_file_contents(file_path, minify_css)
+    elif file_path.endswith(".js"):
+        print(f"Minifying {file_path}")
+        replace_file_contents(file_path, minify_js)
+
 
 def process_recursive(dir_path: str):
     for root, _dirs, files in os.walk(dir_path):
         for name in files:
             file_path = os.path.join(root, name)
-            process_file(file_path)                
+            process_file(file_path)
+
 
 if __name__ == "__main__":
     process_recursive(".")
